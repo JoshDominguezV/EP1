c465a899f83f48bdf1f621ad5c2ca61b6d370bec
josue <jdomiinguezz@gmail.com>
2023-03-25 20:25:48 -0600

    EP1

--- conection_parameters.py
+++ conection_parameters.py
@@ -4,4 +4,12 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["ep1-c"]
 collection = db["parcial1"]
 
+# Verificar si la colección ya existe
+if "parcial1" not in db.list_collection_names():
+    # Si no existe, crearla y agregar los documentos
+    parcial1.insert_many(data)
+    print("La base de datos y la colección han sido creadas exitosamente!")
+else:
+    print("La colección ya existe en la base de datos.")
+
 
--- index.py
+++ index.py
@@ -1,33 +1,119 @@
 from crud import *
 from basic_functions import *
+
 while True:
     print("Desea crear la base de datos? si la base de datos ya esta creada escoja la opcion 2 ")
     print("1. SI")
     print("2. NO, seguir al menu.")
     print("3. Salir")
     opcion1 = input("Ingrese una opción: ")
+    print("\n\n")
     if opcion1 == "1":
-        import pymongo
-        import json
+        from mongo import *
+        from conection_parameters import *
+        # Verificar si la colección ya existe
+        if "parcial1" not in db.list_collection_names():
+            # Si no existe, crearla y agregar los documentos
+            parcial1.insert_many(data)
+            print("La base de datos y la colección han sido creadas exitosamente! \n")
+            while True:
+                print("Menu de opciones")
+                print("1. Ver todos los productos")
+                print("2. Filtrar productos por categoría")
+                print("3. Buscar un producto por nombre")
+                print("4. Agregar un producto")
+                print("5. Modificar un producto")
+                print("6. Eliminar un producto")
+                print("7. Salir del sistema")
+                opcion = input("Ingrese una opción: ")
 
-        # Crear una conexión con el servidor de MongoDB
-        client = pymongo.MongoClient("mongodb://localhost:27017/")
+                if opcion == "1":
+                    read_products()
+                    print("\n\n")
+                elif opcion == "2":
+                    category = input("Ingrese la categoría a filtrar: ")
+                    filter_products(category)
+                    print("\n\n")
 
-        # Crear una base de datos llamada "ep1-c"
-        mydb = client["ep1-c"]
+                elif opcion == "3":
+                    name = input("Ingrese el nombre del producto a buscar: ")
+                    read_products(name)
+                    print("\n\n")
 
-        # Crear una colección llamada "parcial1" en la base de datos
-        parcial1 = mydb["parcial1"]
+                elif opcion == "4":
+                    product = create_product_json()
+                    create_product(product)
+                    print("\n\n")
 
-        # Leer los datos del archivo JSON
-        with open('data.json') as file:
-            data = json.load(file)
+                elif opcion == "5":
+                    name = input("Ingrese el nombre del producto a modificar: ")
+                    product_update = json_product_update()
+                    update_product(name, product_update)
+                    print("\n\n")
 
-        # Insertar los datos en la colección "parcial1"
-        parcial1.insert_many(data)
+                elif opcion == "6":
+                    name = input("Ingrese el nombre del producto a eliminar: ")
+                    delete_product(name)
+                    print("\n\n")
 
-        # Imprimir el número de documentos insertados
-        print(parcial1.count_documents({}))
+                elif opcion == "7":
+                    print("Saliendo del sistema...")
+                    print("\n\n")
+                    break
+
+                else:
+                    print("Opción no válida. Intente nuevamente.\n")
+        else:
+            print("La colección ya existe en la base de datos.\n")
+            while True:
+                print("Menu de opciones")
+                print("1. Ver todos los productos")
+                print("2. Filtrar productos por categoría")
+                print("3. Buscar un producto por nombre")
+                print("4. Agregar un producto")
+                print("5. Modificar un producto")
+                print("6. Eliminar un producto")
+                print("7. Salir del sistema")
+                opcion = input("Ingrese una opción: ")
+
+                if opcion == "1":
+                    read_products()
+                    print("\n\n")
+                elif opcion == "2":
+                    category = input("Ingrese la categoría a filtrar: ")
+                    filter_products(category)
+                    print("\n\n")
+
+                elif opcion == "3":
+                    name = input("Ingrese el nombre del producto a buscar: ")
+                    read_products(name)
+                    print("\n\n")
+
+                elif opcion == "4":
+                    product = create_product_json()
+                    create_product(product)
+                    print("\n\n")
+
+                elif opcion == "5":
+                    name = input("Ingrese el nombre del producto a modificar: ")
+                    product_update = json_product_update()
+                    update_product(name, product_update)
+                    print("\n\n")
+
+                elif opcion == "6":
+                    name = input("Ingrese el nombre del producto a eliminar: ")
+                    delete_product(name)
+                    print("\n\n")
+
+                elif opcion == "7":
+                    print("Saliendo del sistema...")
+                    print("\n\n")
+                    break
+
+                else:
+                    print("Opción no válida. Intente nuevamente.\n")
+
+
     elif opcion1 == "2":
         while True:
             print("Menu de opciones")
@@ -42,35 +128,41 @@ while True:
 
             if opcion == "1":
                 read_products()
-
+                print("\n\n")
             elif opcion == "2":
                 category = input("Ingrese la categoría a filtrar: ")
                 filter_products(category)
+                print("\n\n")
 
             elif opcion == "3":
                 name = input("Ingrese el nombre del producto a buscar: ")
                 read_products(name)
+                print("\n\n")
 
             elif opcion == "4":
                 product = create_product_json()
                 create_product(product)
+                print("\n\n")
 
             elif opcion == "5":
                 name = input("Ingrese el nombre del producto a modificar: ")
                 product_update = json_product_update()
                 update_product(name, product_update)
+                print("\n\n")
 
             elif opcion == "6":
                 name = input("Ingrese el nombre del producto a eliminar: ")
                 delete_product(name)
+                print("\n\n")
 
             elif opcion == "7":
                 print("Saliendo del sistema...")
+                print("\n\n")
                 break
 
             else:
                 print("Opción no válida. Intente nuevamente.\n")
-    elif opcion == "3":
+    elif opcion1 == "3":
         print("Saliendo del sistema...")
         break
     else:
